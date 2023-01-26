527437455fd844cd494879d210a8ac9115c1016e
Jhonatan Iñiguez <73409535+copaTech-98@users.noreply.github.com>
2023-01-25 22:57:05 -0500

    feat(notificaciones)

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -7,26 +7,31 @@ import PasswordRegisterInput from "./Inputs/PasswordRegisterInput";
 import PasswordVerifyInput from "./Inputs/PasswordVerify";
 import { Button } from "@mui/material";
 const Register = () => {
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
-    <Box className="Login-form-control"
-    display="flex"
-    flexDirection="column"
-    justifyContent="center">
-     <NameInput/>
-     <LastNameInput/>
-      <AgeInput />
+    <Box
+      className="Login-form-control"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+    >
+      <NameInput />
+      <LastNameInput />
+      <AgeInput handleChange={handleChange} />
       <CountrySelect />
-      <PasswordRegisterInput/>
-      <PasswordVerifyInput/>
+      <PasswordRegisterInput />
+      <PasswordVerifyInput />
       <Button
-          variant="contained"
-          color="secondary"
-          // startIcon={<Icon className="IconL" icon="ph:sign-in-light" />}
-          className="Form-button register-button"
-          // onClick={handleSubmit}
-        >
-          Register
-        </Button>
+        variant="contained"
+        color="secondary"
+        // startIcon={<Icon className="IconL" icon="ph:sign-in-light" />}
+        className="Form-button register-button"
+        // onClick={handleSubmit}
+      >
+        Register
+      </Button>
     </Box>
   );
 };
