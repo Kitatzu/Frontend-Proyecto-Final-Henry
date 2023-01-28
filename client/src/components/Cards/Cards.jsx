16772a55d06ffbcb7ee165797cd379d6833eeee6
javixlive <javixlive@hotmail.com>
2023-01-28 11:55:11 -0600

    changes

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -22,22 +22,29 @@ const Cards = ({ key, id, description, img, name, price, rating }) => {
        target="_parent" 
        rel="noopener noreferer"
     >
-    <div key={key}>
-      <Card sx={{ width: 210, padding: "10px", background: theme[mode].card }}>
-        <CardActionArea>
-          <CardMedia component="img" height="110" image={img} alt="image" />
-          <CardContent
-            sx={{ gap: "10px", display: "flex", flexDirection: "column" }}
-          >
-            <Typography
-              gutterBottom
-              variant="h7"
-              component="div"
-              sx={{ color: theme[mode].textPrimary, fontWeight: "bold" }}
+      <div key={key}>
+        <Card
+          sx={{ width: 210, padding: "10px", background: theme[mode].card }}
+        >
+          <CardActionArea>
+            <CardMedia 
+              component="img" 
+              width={"100%"} 
+              image={img} 
+              alt="image" 
+            />
+            <CardContent
+              sx={{ gap: "10px", display: "flex", flexDirection: "column" }}
             >
-              {name.toUpperCase()}
-            </Typography>
-            {/* <Typography
+              <Typography
+                gutterBottom
+                variant="h7"
+                component="div"
+                sx={{ color: theme[mode].textPrimary, fontWeight: "bold" }}
+              >
+                {name.toUpperCase()}
+              </Typography>
+              {/* <Typography
               variant="body2"
               color="text.secondary"
               sx={{ color: theme[mode].textPrimary }}
