da38e1a49755319d118e088c6f1c27ad5c5024f7
Jhonatan Iñiguez <73409535+copaTech-98@users.noreply.github.com>
2023-02-05 13:17:42 -0500

    fixis Login register && userName

--- client/src/Redux/Thunks/register.js
+++ client/src/Redux/Thunks/register.js
@@ -1,5 +1,3 @@
-
-
 import axios from "axios";
 import Swal from "sweetalert2";
 import Global from "../../Global";
@@ -8,26 +6,26 @@ import { setIsLoading, setIsLog, setUserName } from "../Slices";
 export const RegisterUser = (form) => {
   return async (dispatch) => {
     dispatch(setIsLoading(true));
-    return await axios
-
-      ({
-        url :`${Global.URL}/register`,
-        method: "POST",
-        body: form,
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        data: form,
-      })
+    return await axios({
+      url: `${Global.URL}/register`,
+      method: "POST",
+      body: form,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: form,
+    })
       .then((data) => {
         dispatch(setIsLoading(false));
         const userData = {
           userId: data.data.id,
-          userName: form.email,
+          userName: data.data.userName,
+          email: data.data.email,
           name: data.data.firstName,
           lastName: data.data.lastName,
-          rol: data.data.rol,
-          token: data.data.newToken,
+          avatar: form.avatar,
+          token: data.data.token,
+          rol: data.data.role.rol,
         };
         localStorage.setItem("token", JSON.stringify(userData));
         Swal.fire({
